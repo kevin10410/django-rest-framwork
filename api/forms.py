159d4efa0e14990de78d9ebea330a11445ded526
kevin10410 <kevin10410@gmail.com>
2019-09-29 16:47:41 +0800

    add validation for content or image

--- api/forms.py
+++ api/forms.py
@@ -11,3 +11,15 @@ class StatusForm(forms.ModelForm):
         'content',
         'image',
       ]
+
+    def clean(self):
+        data = self.cleaned_data
+        content = data.get('content', None)
+
+        content = None if content == "" else content
+        image = data.get('image', None)
+
+        if content == None and image == None:
+          raise forms.ValidationError('Content or image is required')
+
+        return super().clean()
