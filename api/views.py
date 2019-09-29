e172de2932de03a77c72c1066f52cb8ce2e56d72
kevin10410 <kevin10410@gmail.com>
2019-09-29 20:45:38 +0800

    add StatusListAPIView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .models import Status
+from .serializers import StatusSerialzer
 # Create your views here.
+
+class StatusListAPIView(APIView):
+  permission_classes      = []
+  authentication_classes  = []
+
+  def get(self, request, format=None):
+    allData = Status.objects.all()
+    serializers = StatusSerialzer(allData, many=True)
+    return Response(serializers.data)
