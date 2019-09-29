e6d19ab09ce7445ab3d9d3738ab2a0959d91bc57
kevin10410 <kevin10410@gmail.com>
2019-09-29 20:43:34 +0800

    add urls setting for api app

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import StatusListAPIView
+
+urlpatterns = [
+  path('', StatusListAPIView.as_view()),
+  # path('create/', StatusCreateAPIView.as_view()),
+  # path('<int:id>/', StatusDetailAPIView.as_view()),
+  # path('<int:id>/update/', StatusUpdateAPIView.as_view()),
+  # path('<int:id>/delete/', StatusDeleteAPIView.as_view()),
+]
