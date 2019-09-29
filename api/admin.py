86fbd5f6c0f3a39c60c09846ead6ec5c4e3920ed
kevin10410 <kevin10410@gmail.com>
2019-09-29 16:54:43 +0800

    register StatusForm to admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .forms import StatusForm
 from .models import Status
 
-admin.site.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+  form = StatusForm
+  class Meta:
+    model: Status
+
+admin.site.register(Status, StatusAdmin)
