25de3bcab218145be74e95856709be4ccfe9ea22
kevin10410 <kevin10410@gmail.com>
2019-09-29 23:21:44 +0800

    replace APIView with restframe generics

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import StatusListAPIView, StatusDetailAPIView
 
 urlpatterns = [
   path('', StatusListAPIView.as_view()),
-  path('<int:id>/', StatusDetailAPIView.as_view()),
+  path('<int:pk>/', StatusDetailAPIView.as_view()),
   # path('create/', StatusCreateAPIView.as_view()),
   # path('<int:id>/update/', StatusUpdateAPIView.as_view()),
   # path('<int:id>/delete/', StatusDeleteAPIView.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,24 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Status
 from .serializers import StatusSerialzer
 # Create your views here.
 
-class StatusListAPIView(APIView):
+class StatusListAPIView(generics.ListAPIView):
   permission_classes      = []
   authentication_classes  = []
+  queryset                = Status.objects.all()
+  serializer_class        = StatusSerialzer
 
-  def get(self, request, format=None):
-    allData = Status.objects.all()
-    serializers = StatusSerialzer(allData, many=True)
-    return Response(serializers.data)
+  # def get(self, request, format=None):
+  #   allData = Status.objects.all()
+  #   serializers = StatusSerialzer(allData, many=True)
+  #   return Response(serializers.data)
 
-class StatusDetailAPIView(APIView):
+class StatusDetailAPIView(generics.RetrieveAPIView):
   permission_classes      = []
   authentication_classes  = []
+  queryset                = Status.objects.all()
+  serializer_class        = StatusSerialzer
 
-  def get(self, request, id, format=None):
-    data = Status.objects.get(pk=id)
-    serializers = StatusSerialzer(data)
-    return Response(serializers.data)
+  # def get(self, request, id, format=None):
+  #   data = Status.objects.get(pk=id)
+  #   serializers = StatusSerialzer(data)
+  #   return Response(serializers.data)
